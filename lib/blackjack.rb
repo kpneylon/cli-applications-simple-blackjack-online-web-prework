f4c53a0a56e17eaa318894f4a57aee68d10c3b36
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-30 05:25:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,12 +31,10 @@ end
 def hit?(card_total)
   prompt_user 
   user_input = get_user_input
-  if user_input == "h"
-    card_total += deal_card 
-  elsif user_input == "s"
-    card_total
-  else
-    invalid_command
+  if user_input == "s"
+    current_card_total 
+  elsif user_input == "h"
+    deal_card + current_card_total
   end
 end
 
